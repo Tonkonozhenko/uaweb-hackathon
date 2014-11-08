2f56f2e42fa452f3cb07f6abf34fcc18cf63b277
ZitRos <zitros.lab@gmail.com>
2014-11-09 00:01:44 +0200

    register / login ready

---
+++ app/assets/javascripts/Comments.js
@@ -0,0 +1,27 @@
+/**
+ * @param {App} app
+ * @constructor
+ */
+var Comments = function (app) {
+
+    this.app = app;
+
+    this.init();
+
+};
+
+Comments.prototype.init = function () {
+
+    var _ = this;
+
+    $("#comment-submit").click(function () {
+
+        $.post("news/" + _.app.newsPage.currentID + "/comments.json", {
+            "comment[text]": $("#comment-textarea").val()
+        }).done(function (data) {
+            console.log("COMMENT:", data);
+        });
+
+    });
+
+};
