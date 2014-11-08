85b15aeab0658ac851162cc57684fd807eec65e4
Alex Tonkonozhenko <tonkonozhenko@gmail.com>
2014-11-08 18:06:10 +0200

    serializer

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -2,4 +2,26 @@ class NewsItemsController < ApplicationController
   inherit_resources
   actions :index, :show
   respond_to :json, :html
+
+  before_filter :authenticate_user!, only: [:like, :dislike]
+
+  def like
+    ok =
+        if resource.liked_by?(current_user) || resource.disliked_by?(current_user)
+          false
+        else
+          resource.plus!(current_user_id)
+        end
+    render json: { ok: ok }, status: ok ? :ok : :forbidden
+  end
+
+  def dislike
+    current_user_id = current_user.id
+
+    ok =
+        if resource.plus_ids.index(current_user_id).nil? && resource.minus_ids.index(current_user_id).nil?
+          resource.minus!(current_user_id)
+        end
+    render json: { ok: ok }, status: ok ? :ok : :forbidden
+  end
 end
--- app/serializers/news_item_serializer.rb
+++ app/serializers/news_item_serializer.rb
@@ -1,3 +1,12 @@
 class NewsItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :text, :url
+  attributes :id, :title, :text, :url, :short_text, :liked, :disliked
+  delegate :current_user, to: :scope
+
+  def liked
+    object.liked_by?(scope)
+  end
+
+  def disliked
+    object.disliked_by?(scope)
+  end
 end
